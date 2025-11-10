aced820429ef9eb7353d62f70540c079d563cdaf
manish <chyauniyalmanish@gmail.com>
2025-11-10 23:28:28 +0530

    Create user schema model

---
+++ src/model/user.model.ts
@@ -0,0 +1,54 @@
+import { model, Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+      type: String,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    phone: {
+      type: Number,
+      unique: true,
+      sparse: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      sparse: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+    isPhoneVerified: {
+      type: Boolean,
+      default: false,
+    },
+    photoUrl: {
+      type: String,
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female", "other"],
+    },
+    dob: {
+      type: Date,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = model("User", userSchema);
+export default User;
