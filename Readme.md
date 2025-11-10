556e67cd613a424b8d802fcd8247817068dff09b
manish <chyauniyalmanish@gmail.com>
2025-11-10 23:50:51 +0530

    wip: signup controller

--- Readme.md
+++ Readme.md
@@ -11,3 +11,11 @@
 - nodemon --exec ts-node --esm src/app.ts (--esm for esmodule) (explicitly tell nodemon to use ts-node)
 - nodemon --esm src/app.ts (nodemon internally use ts-node)
 - use tsconfig to allow .ts imports
+
+# MongoDB
+
+- schema if field is unique but not required (e.g Phone)
+  - cause duplicate error of null value
+  - e.g phone -> no 2 record can be inserted with null value
+  - to solve -> sparse:true it will ignore field with null or undefined values
+  - IMP: update new sparse:true index by deleting old index in mongodb compass
--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,5 +1,45 @@
-import type { Request, Response } from "express";
+import type { NextFunction, Request, Response } from "express";
+import User from "../model/user.model.ts";
+import { BadRequest } from "../utils/apiError.ts";
 
-export const signup = (req: Request, res: Response) => {
-  res.send("Signup");
+export const signup = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { username, email, phone, firstname, lastname, password } = req.body;
+    if (!username || !(email || phone) || !firstname || !lastname || !password)
+      throw new BadRequest("All Fields are mandatory!!!");
+
+    if (email && phone)
+      throw new BadRequest(
+        "Please provide either your email address or phone number to continue"
+      );
+
+    const usernameExist = await User.findOne({ username });
+    if (usernameExist) throw new BadRequest("Username already exist!!!");
+
+    const query: { email?: string; phone?: number } = {};
+    if (email) query.email = email;
+    if (phone) query.phone = phone;
+
+    const userExist = await User.findOne(query);
+    if (userExist) throw new BadRequest("User already exist!!!");
+
+    const newUser = new User({
+      firstname,
+      lastname,
+      username,
+      email,
+      phone,
+      password,
+    });
+
+    await newUser.save();
+
+    res.send("Signup");
+  } catch (error) {
+    next(error);
+  }
 };
