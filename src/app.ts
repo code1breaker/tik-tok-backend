838e6a0e562c895289e066ccec5375bb7c9855ad
manish <chyauniyalmanish@gmail.com>
2025-11-10 23:37:09 +0530

    global error handling

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,22 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+// routes
 import authRouter from "./routes/auth.route.ts";
 
+// middleware
+import errorMiddleware from "./middleware/error.middleware.ts";
+
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(cors());
+app.use(express.json());
+
 app.use("/api/auth", authRouter);
+app.use(errorMiddleware);
 
 app.listen(PORT, () => {
   console.log(`server is listening on port: ${PORT}`);
